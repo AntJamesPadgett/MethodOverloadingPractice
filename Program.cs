b766517729932d2fbeae68d4bbb7546ca05cf21e
antjamespadgett <padgettanthonyj@gmail.com>
2021-11-18 09:06:57 -0600

    Practiced MethodOverloading

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace MethodOverloadingPractice
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            Add(5, 6);
+            Add(5, 6, 7);
+            Add("Hello", "World");
+            Add(5.5, 6.7);
+            Console.ReadLine();
+           
+        }
+
+        public static void Add(int x, int y)
+        {
+            int answer = x + y;
+            Console.WriteLine(answer);
+        }
+        public static void Add(string x, string y)
+        {
+            string answer = x +  y;
+            Console.WriteLine(answer);
+        }
+        public static void Add(double x, double y)
+        {
+            double answer = x + y;
+            Console.WriteLine(answer);
+        }
+        public static void Add( int x, int y, int z)
+        {
+            int answer = x + y + z;
+            Console.WriteLine(answer);
+        }
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
